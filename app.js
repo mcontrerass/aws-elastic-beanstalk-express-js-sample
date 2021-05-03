e0c50bb748b5fc082c85aa1b961be98165c66066
mcontrerass <mcontrerass@gmail.com>
2021-05-02 23:22:44 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Prueba mensaje!'));
+app.get('/', (req, res) => res.send('Prueba mensaje 02 de mayo de 2021!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
